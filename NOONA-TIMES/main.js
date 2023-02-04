0647b8952a68518c1ee84f48291282f211ccc613
biyakim <w2116@e-mirim.hs.kr>
2023-02-04 20:27:30 +0900

    기초 정리 완성

--- NOONA-TIMES/main.js
+++ NOONA-TIMES/main.js
@@ -1,7 +1,12 @@
-
-
-const getLatestNews = () => {
+let news = {}
+const getLatestNews = async() => {
     let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
-    console.log(url)
+    let header = new Headers({'x-api-key':'t9Yut0w6KuD8EYiSxyR0Q5OKVKoxGSg9TQITAq-U92c'})
+
+    let response = await fetch(url,{headers:header}) //ajax, http, fetch
+    let data = await response.json()
+    news = data.articles
+    console.log(news)
+    
 }
 getLatestNews()
